1e2c6bb5a78c0536d54d4c63b0efc9c191128736
JStibenpb <jstibenpb@gmail.com>
2020-07-21 18:14:03 -0500

    Testing mirror to Azure

--- app.js
+++ app.js
@@ -29,7 +29,6 @@ app.use((req, res, next) => {
   // to the API (e.g. in case you use sessions)
   res.setHeader('Access-Control-Allow-Credentials', true);
 
-  // Pass to next layer of middleware
   next();
 });
 
